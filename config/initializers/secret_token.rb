8f4178b800efd97489fb03c8d178fb8242aec99d
Koichiro Ohba <koichiro@meadowy.org>
2013-10-26 20:03:53 +0900

    Upgrade Rails4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Jrubyci::Application.config.secret_token = '27e2cbfb7270ca7183c95c8e4d573a6af9fbed385a8a6dca1a9f7aa034c590af05201758cc2fed92ad9d1b5457905a01feec497b651ab0e3ffa69db2951e27c1'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jrubyci::Application.config.secret_key_base = '6724bfb209107d5d060c06ca7c166a1fd7066f349ffb201ca1da1f99808b685c7c9027eedbe5186b3c17bde5dc7da152e393f0f723f1436cb346eeea522a9054'
